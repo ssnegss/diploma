e9d17f94e5c15c8f2e5569b6b267881000907e02
julia snegova <ssnnggvv@gmail.com>
2023-03-30 20:12:30 +0300

    Test d3 line chart is added

--- src/components/GraphVisualizationComponent/GraphVizualizationComponent.jsx
+++ src/components/GraphVisualizationComponent/GraphVizualizationComponent.jsx
@@ -1,3 +1,10 @@
+import { TestLineChart } from "../LineChart/TestChart";
+
 export const Graph = () => {
-   return <h1> hello graaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaph</h1>;
+   return (
+      <>
+         <h1> hello graaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaph</h1>
+         <TestLineChart />
+      </>
+   );
 };
